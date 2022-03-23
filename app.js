497cd68cf067cfbefd330ea8e108d0af4a291901
moon115599 <76196460+moon115599@users.noreply.github.com>
2022-03-23 10:08:49 +0900

    add painting function

---
+++ app.js
@@ -0,0 +1,40 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = canvas.offsetWidth;
+canvas.heigth = canvas.offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 1;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
